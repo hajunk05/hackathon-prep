c0c25d66a390b236e9aa856f26c5b9b636e9cda5
hajunk05 <hajunk05@gmail.com>
2025-04-11 18:26:17 +1200

    GET /notes

---
+++ backend/src/main/java/com/example/demo/Note.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+/**
+ * {
+ *     title: ,
+ *     body: ,
+ * }
+ */
+public class Note {
+    public String title;
+    public String body;
+
+
+    public Note(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+}
---
+++ backend/src/main/java/com/example/demo/NoteController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class NoteController {
+
+    // GET /hello -> "Hello, world!"
+    @GetMapping("/hello")
+    public List<Note> sayHello() {
+        
+        return List.of(new Note("some title1", "some body1"), new Note("some title2", "some body2"));
+    }
+
+
+}
